20aa636dbbe796403a8e246bf69d07316337ac79
Mohit Bagdi <92544103+edito0@users.noreply.github.com>
2022-07-25 19:22:17 +0530

    .

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,73 @@
 import React from 'react'
 import './home.css'
+import logo from '../../images/logo.png'
+import headerImage from '../../images/Untitled-1.png'
 
 const Home = () => {
+
+
+  function menu() {
+    const humbuerger = document.querySelector('.humburger');
+    const humbuergerLine = document.querySelectorAll('.humburger-line');
+
+    humbuergerLine.forEach((element) => {
+      element.classList.toggle('menu');
+    });
+
+    const navbar = document.querySelector('.nav__list__container')
+    navbar.classList.toggle('nav__list__container-menu');
+    humbuerger.classList.toggle('humburger-menu');;
+
+
+  }
+
+
   return (
-    <main>Home</main>
+    <>
+      <header class="home__header">
+        <nav>
+          <div class="nav__logo__container">
+            <img src={logo} alt="" />
+          </div>
+
+          <div class="nav__list__container">
+            <ul>
+              <li><a href="index.html">Home</a> <div className='active'></div></li>
+              <li><a href="about.html">About Us</a><div></div></li>
+              <li><a href="gallery.html">Our Team</a><div></div></li>
+              <li><a href="contact-us.html">Events</a><div></div></li>
+              <li><a href="contact-us.html">Contact Us</a><div></div></li>
+            </ul>
+          </div>
+
+        </nav>
+
+        <div className='header__container'>
+          <div className='header__image__container'>
+            <img src={headerImage} alt="" />
+          </div>
+
+          <div className='header__data__container'>
+            <h1>WE STRIVE TO <br /> SUCCESS</h1>
+            <p>In Association with lIT UNA</p>
+            <div> 
+              <button className='btn-primary'>Know More</button>
+              <button className='btn-secondary'>Contact Us</button>
+            </div>
+          </div> 
+        </div>
+
+        <div class='humburger' onClick={menu}>
+          <div class='humburger-line'></div>
+          <div class='humburger-line'></div>
+          <div class='humburger-line'></div>
+        </div>
+
+
+
+
+      </header>
+    </>
   )
 }
 
